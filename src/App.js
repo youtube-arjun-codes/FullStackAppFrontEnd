bbf5338043be4d57705eca65c0bdac5bc772fa00
arjungautam1 <arjungautam5431@gmail.com>
2021-07-07 21:56:54 +0545

    save student to database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import Appbar from './components/Appbar'
+import Student from './components/Student'
 
 function App() {
   return (
     <div className="App">
     <Appbar/>
+    <Student/>
+   
     </div>
   );
 }
---
+++ src/components/Student.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Container ,Paper,Button} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+     
+    },
+  },
+}));
+
+export default function Student() {
+    const paperStyle={padding:'50px 20px', width:600,margin:"20px auto"}
+    const[name,setName]=useState('')
+    const[address,setAddress]=useState('')
+  const classes = useStyles();
+
+  const handleClick=(e)=>{
+    e.preventDefault()
+    const student={name,address}
+    console.log(student)
+    fetch("http://localhost:8080/student/add",{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify(student)
+
+  }).then(()=>{
+    console.log("New Student added")
+  })
+}
+
+  return (
+
+    <Container>
+        <Paper elevation={3} style={paperStyle}>
+            <h1 style={{color:"blue"}}><u>Add Student</u></h1>
+
+    <form className={classes.root} noValidate autoComplete="off">
+    
+      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth 
+      value={name}
+      onChange={(e)=>setName(e.target.value)}
+      />
+      <TextField id="outlined-basic" label="Student Adress" variant="outlined" fullWidth
+      value={address}
+      onChange={(e)=>setAddress(e.target.value)}
+      />
+      <Button variant="contained" color="secondary" onClick={handleClick}>
+  Submit
+</Button>
+    </form>
+    {name}
+    {address}
+    </Paper>
+    </Container>
+  );
+}
