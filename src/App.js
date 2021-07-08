6fc7c715455a57516ae7ba48bd2615c544b26194
arjungautam1 <arjungautam5431@gmail.com>
2021-07-08 16:20:36 +0545

    completed

--- src/App.js
+++ src/App.js
@@ -14,3 +14,4 @@ function App() {
 }
 
 export default App;
+
--- src/components/Student.js
+++ src/components/Student.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import { Container ,Paper,Button} from '@material-ui/core';
@@ -16,7 +16,8 @@ export default function Student() {
     const paperStyle={padding:'50px 20px', width:600,margin:"20px auto"}
     const[name,setName]=useState('')
     const[address,setAddress]=useState('')
-  const classes = useStyles();
+    const[students,setStudents]=useState([])
+     const classes = useStyles();
 
   const handleClick=(e)=>{
     e.preventDefault()
@@ -32,6 +33,14 @@ export default function Student() {
   })
 }
 
+useEffect(()=>{
+  fetch("http://localhost:8080/student/getAll")
+  .then(res=>res.json())
+  .then((result)=>{
+    setStudents(result);
+  }
+)
+},[])
   return (
 
     <Container>
@@ -52,9 +61,27 @@ export default function Student() {
   Submit
 </Button>
     </form>
-    {name}
-    {address}
+   
     </Paper>
+    <h1>Students</h1>
+
+    <Paper elevation={3} style={paperStyle}>
+
+      {students.map(student=>(
+        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left"}} key={student.id}>
+         Id:{student.id}<br/>
+         Name:{student.name}<br/>
+         Address:{student.address}
+
+        </Paper>
+      ))
+}
+
+
+    </Paper>
+
+
+
     </Container>
   );
 }
