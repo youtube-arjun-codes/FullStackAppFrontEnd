d2002eac3d4c39513acde64b27d5f7fbcbf209b4
Brian Su <briansu2004@hotmail.com>
2022-12-30 09:20:18 -0500

    Fix not refreshing issue
    After saving a new student, the all students section are not auto-refreshed.

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,87 +1,94 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import { Container ,Paper,Button} from '@material-ui/core';
+import { Container, Paper, Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
-     
     },
   },
 }));
 
 export default function Student() {
-    const paperStyle={padding:'50px 20px', width:600,margin:"20px auto"}
-    const[name,setName]=useState('')
-    const[address,setAddress]=useState('')
-    const[students,setStudents]=useState([])
-     const classes = useStyles();
+  const paperStyle = { padding: '50px 20px', width: 600, margin: '20px auto' };
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [students, setStudents] = useState([]);
+  const classes = useStyles();
 
-  const handleClick=(e)=>{
-    e.preventDefault()
-    const student={name,address}
-    console.log(student)
-    fetch("http://localhost:8080/student/add",{
-      method:"POST",
-      headers:{"Content-Type":"application/json"},
-      body:JSON.stringify(student)
+  const handleClick = (e) => {
+    e.preventDefault();
+    const student = { name, address };
+    console.log(student);
+    fetch('http://localhost:8080/student/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(student),
+    }).then(() => {
+      console.log('New Student added');
+      refreshAllStudents();
+    });
+  };
 
-  }).then(()=>{
-    console.log("New Student added")
-  })
-}
+  const refreshAllStudents = () => {
+    fetch('http://localhost:8080/student/getAll')
+      .then((res) => res.json())
+      .then((result) => {
+        setStudents(result);
+      });
+  };
 
-useEffect(()=>{
-  fetch("http://localhost:8080/student/getAll")
-  .then(res=>res.json())
-  .then((result)=>{
-    setStudents(result);
-  }
-)
-},[])
+  useEffect(() => {
+    refreshAllStudents();
+  }, []);
   return (
-
     <Container>
-        <Paper elevation={3} style={paperStyle}>
-            <h1 style={{color:"blue"}}><u>Add Student</u></h1>
+      <Paper elevation={3} style={paperStyle}>
+        <h1 style={{ color: 'blue' }}>
+          <u>Add Student</u>
+        </h1>
 
-    <form className={classes.root} noValidate autoComplete="off">
-    
-      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth 
-      value={name}
-      onChange={(e)=>setName(e.target.value)}
-      />
-      <TextField id="outlined-basic" label="Student Adress" variant="outlined" fullWidth
-      value={address}
-      onChange={(e)=>setAddress(e.target.value)}
-      />
-      <Button variant="contained" color="secondary" onClick={handleClick}>
-  Submit
-</Button>
-    </form>
-   
-    </Paper>
-    <h1>Students</h1>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField
+            id="outlined-basic"
+            label="Student Name"
+            variant="outlined"
+            fullWidth
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Student Adress"
+            variant="outlined"
+            fullWidth
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <Button variant="contained" color="secondary" onClick={handleClick}>
+            Submit
+          </Button>
+        </form>
+      </Paper>
+      <h1>Students</h1>
 
-    <Paper elevation={3} style={paperStyle}>
-
-      {students.map(student=>(
-        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left"}} key={student.id}>
-         Id:{student.id}<br/>
-         Name:{student.name}<br/>
-         Address:{student.address}
-
-        </Paper>
-      ))
-}
-
-
-    </Paper>
-
-
-
+      <Paper elevation={3} style={paperStyle}>
+        {students.map((student) => (
+          <Paper
+            elevation={6}
+            style={{ margin: '10px', padding: '15px', textAlign: 'left' }}
+            key={student.id}
+          >
+            Id:{student.id}
+            <br />
+            Name:{student.name}
+            <br />
+            Address:{student.address}
+          </Paper>
+        ))}
+      </Paper>
     </Container>
   );
 }
